513d1525411fdb9ddfaf07ef209953bdd2a61595
s2it_jsilva <s2it_jsilva@uolinc.com>
2017-09-18 09:33:10 -0300

    Issue: can't handle with phones without DDD (area code), this commit support that now

--- src/phone.js
+++ src/phone.js
@@ -1,22 +1,34 @@
 /*exported PHONE */
 var PHONE = function(value) {
-	var phoneMask8D = new StringMask('(00) 0000-0000'),
-		phoneMask9D = new StringMask('(00) 00000-0000'),
-		phoneMask0800 = new StringMask('0000-000-0000');
+	 var formatedValue;
+	var phoneMask8D = {
+        areaCode: new StringMask('(00) 0000-0000'),     // with area code
+        simple: new StringMask('0000-0000')             // without area code
+    }, phoneMask9D = {
+        areaCode: new StringMask('(00) 00000-0000'),    // with area code
+        simple: new StringMask('00000-0000')            // without area code
+    }, phoneMask0800 = {
+        areaCode: null,                                 // N/A
+        simple: new StringMask('0000-000-0000')         // N/A, so it's "simple"
+    };
 
-	if(!value) {
-		return value;
-	}
+    if(!value) {
+        return value;
+    }
 
-	var formatedValue;
-	value = value + '';
-	if (value.indexOf('0800') === 0) {
-			formatedValue = phoneMask0800.apply(value);
-	}else if(value.length < 11){
-		formatedValue = phoneMask8D.apply(value);
-	}else{
-		formatedValue = phoneMask9D.apply(value);
-	}
+    value = value + '';
 
-	return formatedValue;
+    if (value.indexOf('0800') === 0) {
+        formattedValue = phoneMask0800.simple.apply(value);
+    } else if (value.length < 9) {
+        formattedValue = phoneMask8D.simple.apply(value) || '';
+    } else if (value.length < 10) {
+        formattedValue = phoneMask9D.simple.apply(value);
+    } else if (value.length < 11) {
+        formattedValue = phoneMask8D.areaCode.apply(value);
+    } else {
+        formattedValue = phoneMask9D.areaCode.apply(value);
+    }
+
+    return formattedValue.trim().replace(/[^0-9]$/, '');
 };
